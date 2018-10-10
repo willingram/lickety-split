5139922e0ea0988868ab5459bed186796fb9afa2
William Ingram <william.ingram@concentra.co.uk>
2018-10-10 19:04:05 +0100

    +app.js button to log event to pinpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Auth from '@aws-amplify/auth';
+import Analytics from '@aws-amplify/analytics';
+
+import awsconfig from './aws-exports';
+
+// retrieve temporary AWS credentials and sign requests
+Auth.configure(awsconfig);
+// send analytics events to Amazon Pinpoint
+Analytics.configure(awsconfig);
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleAnalyticsClick = this.handleAnalyticsClick.bind(this);
+    this.state = {analyticsEventSent: false, resultHtml: "", eventsSent: 0};
+  }
+
+  handleAnalyticsClick() {
+      Analytics.record('AWS Amplify Tutorial Event')
+        .then( (evt) => {
+            const url = 'https://console.aws.amazon.com/pinpoint/home/?region=us-east-1#/apps/'+awsconfig.aws_mobile_analytics_app_id+'/analytics/events';
+            let result = (<div>
+              <p>Event Submitted.</p>
+              <p>Events sent: {++this.state.eventsSent}</p>
+              <a href={url} target="_blank">View Events on the Amazon Pinpoint Console</a>
+            </div>);
+            this.setState({
+                'analyticsEventSent': true,
+                'resultHtml': result
+            });
+        });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">Welcome to React</h1>
         </header>
+        <div className="App-intro">
+          <button className="App-button" onClick={this.handleAnalyticsClick}>Generate Analytics Event</button>
+          {this.state.analyticsEventSent}
+          <div>{this.state.resultHtml}</div>
+        </div>
       </div>
     );
   }
